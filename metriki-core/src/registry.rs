385c2c94ad3e67807b8b496eaa65ee41086dc3d9
Ning Sun <sunng@protonmail.com>
2021-02-12 20:27:23 +0800

    (fix) avoid deadlock in metrics registry

--- metriki-core/src/registry.rs
+++ metriki-core/src/registry.rs
@@ -23,74 +23,90 @@ impl MetricsRegistry {
     /// Return `Meter` that has been registered or just created and resgitered.
     /// Panic if a metric is already register but is not meter
     pub fn meter(&self, name: &str) -> Arc<Meter> {
-        let inner = self.inner.read().unwrap();
+        let meter = {
+            let inner = self.inner.read().unwrap();
 
-        if !inner.metrics.contains_key(name) {
+            inner.metrics.get(name).map(|metric| match metric {
+                Metric::Meter(ref m) => m.clone(),
+                _ => panic!("A metric with same name and different type is already registered."),
+            })
+        };
+
+        if let Some(m) = meter {
+            m
+        } else {
             let mut inner_write = self.inner.write().unwrap();
-            let meter = Meter::new();
+            let meter = Arc::new(Meter::new());
             inner_write
                 .metrics
-                .insert(name.to_owned(), Metric::Meter(Arc::new(meter)));
-        }
-
-        let metric = inner.metrics.get(name).unwrap();
-        match metric {
-            Metric::Meter(ref m) => m.clone(),
-            _ => panic!("A metric with same name and different type is already registered."),
+                .insert(name.to_owned(), Metric::Meter(meter.clone()));
+            meter
         }
     }
 
     pub fn histogram(&self, name: &str) -> Arc<Histogram> {
-        let inner = self.inner.read().unwrap();
+        let histo = {
+            let inner = self.inner.read().unwrap();
 
-        if !inner.metrics.contains_key(name) {
+            inner.metrics.get(name).map(|metric| match metric {
+                Metric::Histogram(ref m) => m.clone(),
+                _ => panic!("A metric with same name and different type is already registered."),
+            })
+        };
+
+        if let Some(m) = histo {
+            m
+        } else {
             let mut inner_write = self.inner.write().unwrap();
-            let histogram = Histogram::new();
+            let histo = Arc::new(Histogram::new());
             inner_write
                 .metrics
-                .insert(name.to_owned(), Metric::Histogram(Arc::new(histogram)));
-        }
-
-        let metric = inner.metrics.get(name).unwrap();
-        match metric {
-            Metric::Histogram(ref m) => m.clone(),
-            _ => panic!("A metric with same name and different type is already registered."),
+                .insert(name.to_owned(), Metric::Histogram(histo.clone()));
+            histo
         }
     }
 
     pub fn counter(&self, name: &str) -> Arc<Counter> {
-        let inner = self.inner.read().unwrap();
+        let counter = {
+            let inner = self.inner.read().unwrap();
 
-        if !inner.metrics.contains_key(name) {
+            inner.metrics.get(name).map(|metric| match metric {
+                Metric::Counter(ref m) => m.clone(),
+                _ => panic!("A metric with same name and different type is already registered."),
+            })
+        };
+
+        if let Some(m) = counter {
+            m
+        } else {
             let mut inner_write = self.inner.write().unwrap();
-            let counter = Counter::new();
+            let counter = Arc::new(Counter::new());
             inner_write
                 .metrics
-                .insert(name.to_owned(), Metric::Counter(Arc::new(counter)));
-        }
-
-        let metric = inner.metrics.get(name).unwrap();
-        match metric {
-            Metric::Counter(ref m) => m.clone(),
-            _ => panic!("A metric with same name and different type is already registered."),
+                .insert(name.to_owned(), Metric::Counter(counter.clone()));
+            counter
         }
     }
 
     pub fn timer(&self, name: &str) -> Arc<Timer> {
-        let inner = self.inner.read().unwrap();
+        let timer = {
+            let inner = self.inner.read().unwrap();
 
-        if !inner.metrics.contains_key(name) {
+            inner.metrics.get(name).map(|metric| match metric {
+                Metric::Timer(ref m) => m.clone(),
+                _ => panic!("A metric with same name and different type is already registered."),
+            })
+        };
+
+        if let Some(m) = timer {
+            m
+        } else {
             let mut inner_write = self.inner.write().unwrap();
-            let timer = Timer::new();
+            let timer = Arc::new(Timer::new());
             inner_write
                 .metrics
-                .insert(name.to_owned(), Metric::Timer(Arc::new(timer)));
-        }
-
-        let metric = inner.metrics.get(name).unwrap();
-        match metric {
-            Metric::Timer(ref m) => m.clone(),
-            _ => panic!("A metric with same name and different type is already registered."),
+                .insert(name.to_owned(), Metric::Timer(timer.clone()));
+            timer
         }
     }
 
