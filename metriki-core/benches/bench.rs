1c6da10c3f8ea3bac7bc56ccba485405a2c959d4
Ning Sun <sunng@protonmail.com>
2021-05-02 19:47:09 +0800

    (feat) initial commit of benchmarks

---
+++ metriki-core/benches/bench.rs
@@ -0,0 +1,35 @@
+#![feature(test)]
+extern crate test;
+
+use rand;
+use test::Bencher;
+
+use metriki_core::MetricsRegistry;
+
+#[bench]
+fn bench_meter_mark(b: &mut Bencher) {
+    let rg = MetricsRegistry::arc();
+
+    b.iter(|| rg.meter("test.meter").mark());
+}
+
+#[bench]
+fn bench_histogram_update(b: &mut Bencher) {
+    let rg = MetricsRegistry::arc();
+
+    b.iter(|| {
+        rg.histogram("test.histogram")
+            .update((rand::random::<f64>() * 1000.0) as i64)
+    });
+}
+
+#[bench]
+fn bench_timer(b: &mut Bencher) {
+    let rg = MetricsRegistry::arc();
+
+    b.iter(|| {
+        let timer = rg.timer("test.timer");
+        let ctx = timer.start();
+        ctx.stop();
+    });
+}
