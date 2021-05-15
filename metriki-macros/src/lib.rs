8c16293127d59d3c4c3293be5b178e710fe94c7b
Ning Sun <sunng@protonmail.com>
2021-05-15 22:04:50 +0800

    (refactor) fix unwrap usage

--- metriki-macros/src/lib.rs
+++ metriki-macros/src/lib.rs
@@ -1,5 +1,3 @@
-use std::collections::HashMap;
-
 use proc_macro::TokenStream;
 use proc_macro2::Span;
 use quote::quote;
@@ -7,44 +5,38 @@ use syn::parse::{Parse, ParseStream};
 use syn::punctuated::Punctuated;
 use syn::{parse_macro_input, Expr, ItemFn, Lit, LitStr, Meta, Result as SynResult, Token};
 
-#[derive(Debug)]
 struct TimedAttributes {
-    registry: String,
-    name: String,
+    registry: Expr,
+    name: Option<Lit>,
 }
 
 impl Parse for TimedAttributes {
     /// to parse #[timed(name="...", registry="...")]
     fn parse(input: ParseStream) -> SynResult<Self> {
         let metas = Punctuated::<Meta, Token![,]>::parse_terminated(input)?;
-        let mut hash = HashMap::new();
+        let mut result = TimedAttributes {
+            registry: syn::parse_str("metriki_core::global::global_registry()")?,
+            name: None,
+        };
 
         // convert attribute metas to key-value map
         for i in metas {
             if let Meta::NameValue(mnv) = i {
-                //                    hash.insert(mnv.path, mvn.lit);
                 if mnv.path.is_ident("name") {
                     if let Lit::Str(ref litstr) = mnv.lit {
-                        hash.insert("name", litstr.value());
+                        result.name = Some(Lit::Str(litstr.clone()));
                     }
                 }
 
                 if mnv.path.is_ident("registry") {
                     if let Lit::Str(ref litstr) = mnv.lit {
-                        hash.insert("registry", litstr.value());
+                        result.registry = syn::parse_str(&litstr.value())?;
                     }
                 }
             }
         }
 
-        // TODO: unwrap
-        Ok(TimedAttributes {
-            registry: hash
-                .get("registry")
-                .cloned()
-                .unwrap_or_else(|| "metriki_core::global::global_registry()".to_owned()),
-            name: hash.get("name").cloned().unwrap(),
-        })
+        Ok(result)
     }
 }
 
@@ -62,19 +54,22 @@ pub fn timed(attrs: TokenStream, input: TokenStream) -> TokenStream {
     } = f;
     let stmts = &block.stmts;
 
-    // FIXME: unwrap
-    let registry_expr: Expr = syn::parse_str(&timer_data.registry).unwrap();
-    let name = Lit::Str(LitStr::new(&timer_data.name, Span::call_site()));
+    let registry = timer_data.registry;
+    // use function name by default
+    let name = timer_data
+        .name
+        .unwrap_or_else(|| Lit::Str(LitStr::new(&sig.ident.to_string(), Span::call_site())));
 
     // generated code
     let tokens = quote! {
         #(#attrs)*
         #vis #sig {
-            let __timer = #registry_expr.timer(#name);
+            let __timer = #registry.timer(#name);
             let __timer_ctx = __timer.start();
 
             #(#stmts)*
         }
     };
+
     tokens.into()
 }
--- metriki-macros/tests/test.rs
+++ metriki-macros/tests/test.rs
@@ -1,10 +1,23 @@
 #[macro_use]
 extern crate metriki_macros;
 
+use std::sync::Arc;
+
 use metriki_core::global::global_registry;
+use metriki_core::MetricsRegistry;
 
 #[timed(name = "my.example.f1")]
 fn my_example_f1() {
+    dbg!("456");
+}
+
+#[timed]
+fn my_example_f2() {
+    dbg!("456");
+}
+
+#[timed(registry = "local_registry")]
+fn my_example_f3(local_registry: Arc<MetricsRegistry>) {
     dbg!("456");
 }
 
@@ -13,6 +26,22 @@ fn test_my_example_f1() {
     my_example_f1();
 
     let s = global_registry().snapshots();
-    assert_eq!(1, s.len());
     assert!(s.contains_key("my.example.f1"));
+}
+
+#[test]
+fn test_my_example_f2() {
+    my_example_f2();
+
+    let s = global_registry().snapshots();
+    assert!(s.contains_key("my_example_f2"));
+}
+
+#[test]
+fn test_my_example_f3() {
+    let local_registry = MetricsRegistry::arc();
+    my_example_f3(local_registry.clone());
+
+    let s = local_registry.snapshots();
+    assert!(s.contains_key("my_example_f3"));
 }
