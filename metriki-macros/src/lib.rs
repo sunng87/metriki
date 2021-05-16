aae88e3ea564dd93f4a243b850ebaad95a9a3c19
Ning Sun <sunng@protonmail.com>
2021-05-16 19:20:11 +0800

    (feat) add metered macro

--- metriki-macros/src/lib.rs
+++ metriki-macros/src/lib.rs
@@ -5,16 +5,16 @@ use syn::parse::{Parse, ParseStream};
 use syn::punctuated::Punctuated;
 use syn::{parse_macro_input, Expr, ItemFn, Lit, LitStr, Meta, Result as SynResult, Token};
 
-struct TimedAttributes {
+struct FnMetricsAttributes {
     registry: Expr,
     name: Option<Lit>,
 }
 
-impl Parse for TimedAttributes {
+impl Parse for FnMetricsAttributes {
     /// to parse #[timed(name="...", registry="...")]
     fn parse(input: ParseStream) -> SynResult<Self> {
         let metas = Punctuated::<Meta, Token![,]>::parse_terminated(input)?;
-        let mut result = TimedAttributes {
+        let mut result = FnMetricsAttributes {
             registry: syn::parse_str("metriki_core::global::global_registry()")?,
             name: None,
         };
@@ -43,7 +43,7 @@ impl Parse for TimedAttributes {
 #[proc_macro_attribute]
 pub fn timed(attrs: TokenStream, input: TokenStream) -> TokenStream {
     let f = parse_macro_input!(input as ItemFn);
-    let timer_data = parse_macro_input!(attrs as TimedAttributes);
+    let timer_data = parse_macro_input!(attrs as FnMetricsAttributes);
 
     // function data
     let ItemFn {
@@ -66,6 +66,39 @@ pub fn timed(attrs: TokenStream, input: TokenStream) -> TokenStream {
         #vis #sig {
             let __timer = #registry.timer(#name);
             let __timer_ctx = __timer.start();
+
+            #(#stmts)*
+        }
+    };
+
+    tokens.into()
+}
+
+#[proc_macro_attribute]
+pub fn metered(attrs: TokenStream, input: TokenStream) -> TokenStream {
+    let f = parse_macro_input!(input as ItemFn);
+    let timer_data = parse_macro_input!(attrs as FnMetricsAttributes);
+
+    // function data
+    let ItemFn {
+        attrs,
+        vis,
+        sig,
+        block,
+    } = f;
+    let stmts = &block.stmts;
+
+    let registry = timer_data.registry;
+    // use function name by default
+    let name = timer_data
+        .name
+        .unwrap_or_else(|| Lit::Str(LitStr::new(&sig.ident.to_string(), Span::call_site())));
+
+    // generated code
+    let tokens = quote! {
+        #(#attrs)*
+        #vis #sig {
+            #registry.meter(#name).mark();
 
             #(#stmts)*
         }
