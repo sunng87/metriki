7feff8dfcd36fb1e2dd3d8856dd34791770c7d2d
Ning Sun <sunng@protonmail.com>
2021-09-30 14:46:37 +0800

    test(tracing): add simple example for metriki tracing layer.

--- metriki-tracing/examples/simple.rs
+++ metriki-tracing/examples/simple.rs
@@ -1,7 +1,19 @@
-use metriki_core::MetricsRegistry;
-use metriki_tracing::MetrikiSubscriber;
-use tracing;
+use std::error::Error;
 
-fn main() -> Result<(), Box<Error>> {
-    MetrikiSubscriber::new().init();
+use metriki_core::global::global_registry;
+use metriki_tracing::MetrikiLayer;
+use tracing::Level;
+use tracing_subscriber::layer::SubscriberExt;
+use tracing_subscriber::registry::Registry;
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let layer = MetrikiLayer::new(global_registry());
+    let subscriber = Registry::default().with(layer);
+    tracing::subscriber::set_global_default(subscriber).unwrap();
+
+    let span = tracing::span!(Level::INFO, "demo");
+
+    drop(span);
+
+    Ok(())
 }
