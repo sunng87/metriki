250fcf7bffce2b866c73f4f7fdae2d386b858670
Ning Sun <sunng@protonmail.com>
2021-10-01 10:16:53 +0800

    doc(tracing): add docstrings to tracing module

--- metriki-tracing/src/lib.rs
+++ metriki-tracing/src/lib.rs
@@ -1,3 +1,17 @@
+//! # Metriki as a Tracing Backend
+//!
+//! This library acts as a backend for
+//! [tracing](https://github.com/tokio-rs/tracing). It tracks spans
+//! and events created with tracing API and translates them into
+//! [metriki](https://github.com/sunng87/metriki) concepts.
+//!
+//! * Spans are recorded with metriki timers.
+//! * Events are recorded with metriki meters.
+//!
+//! By using this backend, developers are able to add Metriki and its
+//! exporter/reporter ecosystem into tracing without touching metriki
+//! apis.
+//!
 use std::sync::Arc;
 
 use tracing::Subscriber;
@@ -10,8 +24,21 @@ use tracing_subscriber::{
 use metriki_core::metrics::TimerContextArc;
 use metriki_core::MetricsRegistry;
 
-// A tracing layer that tracks span and events with Metriki timers
-// and meters.
+// A tracing layer to be integrated with tracing_subscriber's
+// Registry.
+//
+// ```no_run
+// // Create MetrikiLayer from metriki registry
+// let layer = MetrikiLayer::new(global_registry());
+//
+// // Create a subscriber with tracing_subscriber's Registry and
+// // configure MetrikiLayer with it
+// let subscriber = Registry::default().with(layer);
+//
+// // Configure the subscriber to tracing
+// tracing::subscriber::set_global_default(subscriber).unwrap();
+// ```
+//
 pub struct MetrikiLayer {
     registry: Arc<MetricsRegistry>,
 }
@@ -22,6 +49,7 @@ where
 {
     fn on_event(&self, event: &Event<'_>, _ctx: Context<'_, S>) {
         // register event as meter
+        // FIXME: better event metrics name
         self.registry.meter(event.metadata().name()).mark();
     }
 
