a3c9e0d1c047ec0c37262fb0572b474103f39c8c
Ning Sun <sunng@protonmail.com>
2021-10-30 15:26:28 +0800

    fix(otlp): fix protobuf import with tonic

--- metriki-otlp-reporter/src/lib.rs
+++ metriki-otlp-reporter/src/lib.rs
@@ -6,7 +6,32 @@ use metriki_core::MetricsRegistry;
 use derive_builder::Builder;
 
 pub mod opentelemetry {
-    tonic::include_proto!("opentelemetry.proto.collector.metrics.v1");
+    pub mod proto {
+        pub mod collector {
+            pub mod metrics {
+                pub mod v1 {
+                    tonic::include_proto!("opentelemetry.proto.collector.metrics.v1");
+                }
+            }
+        }
+        pub mod common {
+            pub mod v1 {
+                tonic::include_proto!("opentelemetry.proto.common.v1");
+            }
+        }
+
+        pub mod metrics {
+            pub mod v1 {
+                tonic::include_proto!("opentelemetry.proto.metrics.v1");
+            }
+        }
+
+        pub mod resource {
+            pub mod v1 {
+                tonic::include_proto!("opentelemetry.proto.resource.v1");
+            }
+        }
+    }
 }
 
 #[derive(Builder, Debug)]
